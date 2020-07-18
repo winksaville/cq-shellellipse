7ca19c3cd7d54a6022997188fc73620b5534c36e
Wink Saville <wink@saville.com>
2020-07-17 17:53:28 -0700

    Changed to a 40mm x 40mm ellipse

--- shellellipse.py
+++ shellellipse.py
@@ -1,12 +1,19 @@
 import cadquery as cq
 result = (
     cq.Workplane("front")
-    .ellipse(2, 4)
-    .extrude(4)
+    .ellipse(10, 20)
+    .extrude(40)
     .faces(">Z")
-    #.shell(-0.99999) # OK
-    #.shell(-1.00000) # StdFail_NotDone: BRep_API: command not done
-    #.shell(-1.00001)  # weird
-    .shell(-1.7) # very odd
+    #.shell(-0.30) # OK
+    #.shell(-4.99999) # OK
+    #.shell(-5.00000) # StdFail_NotDone: BRep_API: command not done
+    #.shell(-5.00001)  # weird
+    .shell(-9) # very odd
 )
 show_object(result)
+
+#import io
+#tolerance=0.001
+#f = io.open(f'shellellipse-direct-{tolerance}.stl', 'w+')
+#cq.exporters.exportShape(result, cq.exporters.ExportTypes.STL, f, tolerance)
+#f.close()
